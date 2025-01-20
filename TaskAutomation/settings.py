c014f1cee74c57ceda45a8b53692b50b7d95bdef
Prabhjot Kaur <prabhjot@dnagrowht.com>
2025-01-20 19:27:08 +0530

    update yaml file

--- TaskAutomation/settings.py
+++ TaskAutomation/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET")
+SECRET_KEY = 'django-insecure-f#=wt%s$l1zwa%l(0gwme@w2da(6q#eng3n9y@sn9g_ohgcs6w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
